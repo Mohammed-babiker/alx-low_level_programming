09744d7f9de94e2ed8a014988d9d443f750e1a5e
Mohammed-babiker <mohmed9655@gmail.com>
2023-09-14 02:19:56 -0700

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - pwrites the character c to stdout
+ *
+ * on error, -1 return.
+ * Return: on success 1
+ */
+ int _putcha(char c)
+ {
+	 return (write(1, &c, 1));
+ }
