48098e3edd044db4041b03473532465614dfdc31
Mohammed-babiker <mohmed9655@gmail.com>
2023-10-02 23:19:19 -0700

    AA

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -4,11 +4,11 @@
 #include <string.h>
 
 /**
- * * check_num - check - string there are digit
- * * @str: array str
- * *
- * * Return: Always 0 (Success)
- */
+* check_num - check - string there are digit
+* @str: array str
+*
+* Return: Always 0 (Success)
+*/
 
 int check_num(char *str)
 
@@ -31,11 +31,11 @@ int check_num(char *str)
 }
 
 /**
- * main - Print the name of the program
- * @argc : Count arguments
- * @argv : Arguments
- * Return : Always 0 (Success)
- */
+* main - Print the name of the program
+* @argc : Count arguments
+* @argv : Arguments
+* Return : Always 0 (Success)
+*/
 
 int main(int argc, char *argv[])
 {
